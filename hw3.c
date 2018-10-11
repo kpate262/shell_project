de78f36fc2cc6050724a5719184b414c82893fee
Kisan Patel <kpate262@uic.edu>
2018-10-11 09:50:03 -0500

    started

--- hw3.c
+++ hw3.c
@@ -1,12 +1,58 @@
-#include <stdio.h>
-#include <string.h>
+#include "stdlib.h"
+#include "stdio.h"
+#include <unistd.h>
+#include "string.h"
 
-int main(){
-    char line[500];
-    char argsarray[20][100];
 
-    printf("CS361 >"); 
+void forking(char **args1){
+    int pid = fork();
     
+    if(pid == 0){
+      execv(args1[0], args1);
+      exit(0);
+    }else{
+      int status;
+      wait(&status);
+    }
+}
+
+
+int main()
+{
+  char *line;
+  char **args1 = (char**)malloc(sizeof(char*)*20);
+  char prompt[] = "CS361 >";
+  printf("%s", prompt);
+
+  line = (char*)malloc(sizeof(char)*500);
+  fgets(line, 500, stdin);
+  char *commands = strtok(line, " \n");
+
+  int i;
+
+ /* while(1){
+    printf("%s", prompt);*/
+    i = 0;
+    while(commands){
+      printf("%s \n", commands);
+      args1[i] = (char*)malloc(sizeof(char)*100);
+      strcpy(args1[i], commands);
+      i++;
+      if(strncmp(args1[0], "exit",4) == 0){
+        printf("im here");
+        exit(0);
+      }
+      commands = strtok(NULL, " \n");
+    }
+	printf(">%d<", i);
+
+    args1[i] = (char*)0;
+    
+    forking(args1);   
+    /*line = malloc(sizeof(char)*500);
     fgets(line, 500, stdin);
-    
+    commands = strtok(line, " \n");
+    }*/
+
+  return 0;
 }
